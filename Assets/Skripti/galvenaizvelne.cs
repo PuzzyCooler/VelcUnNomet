3c895d598543dc582d5019b59d98a4c1d3e888a8
PuzzyCooler <reinis.net123@gmail.com>
2021-04-30 14:49:10 +0300

    Pievienota galvena izvele+poga

---
+++ Assets/Skripti/galvenaizvelne.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class galvenaizvelne : MonoBehaviour
+{
+    public void izvelne()
+    {
+        SceneManager.LoadScene("PilsetasAina", LoadSceneMode.Single);
+    }
+}
+
+
